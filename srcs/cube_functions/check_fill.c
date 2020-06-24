5c96bb4dbea34a157f0119376646277960c36e56
Yusha <yushabakker1@gmail.com>
2020-06-24 11:56:49 +0200

    problem in flood fill

--- srcs/cube_functions/check_fill.c
+++ srcs/cube_functions/check_fill.c
@@ -6,7 +6,7 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/23 11:13:15 by ybakker       #+#    #+#                 */
-/*   Updated: 2020/06/24 11:48:54 by ybakker       ########   odam.nl         */
+/*   Updated: 2020/06/24 11:54:38 by ybakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,12 @@ int				check_fill(t_struct_m *main)
 	int	x;
 	int	y;
 
-	ft_empty_space(main);
 	x = (int)(main->Ray.posX);
 	y = (int)(main->Ray.posY);
 	main->cubecopy[y][x] = '0';
 	flood_fill(x, y, main);
 	y = 0;
-	while (y < main->Ray.yy)//left
+	while (y <= main->Ray.yy)//left
 	{
 		while (main->cubecopy[y][x])
 		{
@@ -31,31 +30,42 @@ int				check_fill(t_struct_m *main)
 			{
 				main->place.error = 16;
 				ft_error(main);
-				return (0);
+				return (1);
 			}
 			x++;
 		}
 		y++;
 	}
-	while (y < main->Ray.yy)//right
+	while (y <= main->Ray.yy)//right
 	{
 		x = ft_strlen(main->cubecopy[y]);
 		if (main->cubecopy[y][x] == 'X')
 		{
 			main->place.error = 17;
 			ft_error(main);
-			return (0);
+			return (1);
 		}
 		y++;
 	}
 	x = 0;
 	while (main->cubecopy[0][x])//top
 	{
+		if (main->cubecopy[0][x] == 'X')
+		{
+			main->place.error = 17;
+			ft_error(main);
+			return (1);
+		}
+		x++;
+	}
+	y = main->Ray.yy;
+	while (main->cubecopy[y][x])//bottom
+	{
 		if (main->cubecopy[y][x] == 'X')
 		{
 			main->place.error = 17;
 			ft_error(main);
-			return (0);
+			return (1);
 		}
 		x++;
 	}
