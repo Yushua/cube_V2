7cb3749a810a717decfe1fcdc6516e73c5079cf0
Yusha <yushabakker1@gmail.com>
2020-06-30 15:20:59 +0200

    fixing R

--- srcs/cube_functions/readmap/get_info/ft_read_R.c
+++ srcs/cube_functions/readmap/get_info/ft_read_R.c
@@ -6,13 +6,95 @@
 /*   By: ybakker <ybakker@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/06/16 15:08:39 by ybakker       #+#    #+#                 */
-/*   Updated: 2020/06/26 12:03:41 by ybakker       ########   odam.nl         */
+/*   Updated: 2020/06/30 15:20:09 by ybakker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../cube.h"
 
-int			ft_read_r(t_struct_m *main, char *map)
+static	int		ft_read_r_readd(t_struct_m *main, char *map, int i, int r)
+{
+	if (r == 0)
+	{
+		main->place.s_height = ft_atoi_cube(map, main, i);
+		i = main->place.i;
+		main->place.i = 0;
+	}
+	else if (r == 1)
+	{
+		main->place.s_width = ft_atoi_cube(map, main, i);
+		i = main->place.i;
+		main->place.i = 0;
+		main->doublle.d_r = 1;
+	}
+	if (map[i] != '\0' && r == 1)
+	{
+		main->place.error = 28;
+		ft_error(main);
+		return (2);
+	}
+}
+
+static int		ft_read_r_read(t_struct_m *main, char *map, int i, int r)
+{
+	while (map[i] == ' ')
+		i++;
+	if (map[i] != 'R')
+	{
+		main->place.error = 3;
+		ft_error(main);
+	}
+	else
+		i++;
+	while (map[i] == ' ')
+		i++;
+	while (map[i])
+	{
+		while (map[i] == ' ')
+			i++;
+		if (map[i] >= '0' && map[i] <= '9')
+		{
+			i = ft_isalnummer(i, map);
+			if (i < 0)
+			{
+				main->place.error = 27;
+				ft_error(main);
+				return (2);
+			}
+			else
+			{
+				if (r == 0)
+				{
+					main->place.s_height = ft_atoi_cube(map, main, i);
+					i = main->place.i;
+					main->place.i = 0;
+				}
+				else if (r == 1)
+				{
+					main->place.s_width = ft_atoi_cube(map, main, i);
+					i = main->place.i;
+					main->place.i = 0;
+					main->doublle.d_r = 1;
+				}
+				if (map[i] != '\0' && r == 1)
+				{
+					main->place.error = 28;
+					ft_error(main);
+					return (2);
+				}
+			}
+			r++;
+		}
+		else
+		{
+			main->place.error = 3;
+			ft_error(main);
+			return (2);
+		}
+	}
+}
+
+int				ft_read_r(t_struct_m *main, char *map)
 {
 	int		i;
 	int		r;
@@ -26,60 +108,8 @@ int			ft_read_r(t_struct_m *main, char *map)
 	}
 	else
 	{
-		while (map[i] == ' ')
-			i++;
-		if (map[i] != 'R')
-		{
-			main->place.error = 3;
-			ft_error(main);
-		}
-		else
-			i++;
-		while (map[i] == ' ')
-			i++;
-		while (map[i])
-		{
-			while (map[i] == ' ')
-				i++;
-			if (map[i] >= '0' && map[i] <= '9')
-			{
-				i = ft_isalnummer(i, map);
-				if (i < 0)
-				{
-					main->place.error = 27;
-					ft_error(main);
-				}
-				else
-				{
-					if (r == 0)
-					{
-						main->place.s_height = ft_atoi_cube(map, main, i);
-						i = main->place.i;
-						main->place.i = 0;
-					}
-					else if (r == 1)
-					{
-						main->place.s_width = ft_atoi_cube(map, main, i);
-						i = main->place.i;
-						main->place.i = 0;
-						main->doublle.d_r = 1;
-					}
-					if (map[i] != '\0' && r == 1)
-					{
-						main->place.error = 28;
-						ft_error(main);
-						return (0);
-					}
-				}
-				r++;
-			}
-			else
-			{
-				main->place.error = 3;
-				ft_error(main);
-				return (0);
-			}
-		}
+		if (ft_read_r_read(main, map, i, r) == 2)
+			return (0);
 	}
 	if (main->doublle.d_r != 1)
 	{
